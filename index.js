1b18ad7fc4df4a17e102fb93ce1f924fef635a29
Gabriel Castro <gabrieldcc@gmail.com>
2024-09-07 10:23:48 -0300

    mongo db configs

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Question = require('./models/Question');
 const cors = require('cors');
-
 const app = express();
+
+require('dotenv').config(); // Carrega variáveis do arquivo .env
+
 app.use(express.json()); // Middleware para parsing de JSON
 
 // Configuração do CORS
@@ -14,8 +16,13 @@ app.use(cors({
 }));
 
 // Configuração da conexão com o MongoDB
-// const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/questionsdb'; // Fallback para localhost
-const uri = 'mongodb+srv://gabrieldcc:Gabriel98$@cluster0.hs43a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+ const uri = process.env.MONGO_URI 
+
+ if (!uri) {
+  console.error('MONGO_URI não está definida no arquivo .env');
+  process.exit(1);
+}
+//const uri = 'mongodb+srv://gabrieldcc:Gabriel98$@cluster0.hs43a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 mongoose.connect(uri)
   .then(() => {
